6eff7a05f07912387d3b7eb89fe7a115692b94be
han.liu@zoom.us <han.liu@zoom.us>
2024-11-27 16:40:57 +0800

    feat:get hyperlink

--- sheet_alt.go
+++ sheet_alt.go
@@ -1,22 +1,57 @@
 package excelize
 
+import "encoding/xml"
+
 // A monkey-patched	version of sheet.go that provides a function to get all external links in the worksheet,
 // that enables a more flexible way to deal with hyperlinks instead of get hyperlink cell by cell inefficiently.
 
 // Hyperlinks returns a map contains all external links in the worksheet by giving sheet name while cell ref as key, link
 // url as value
 func (f *File) Hyperlinks(sheet string) (map[string]string, error) {
-	links := make(map[string]string)
-	ws, err := f.workSheetReader(sheet)
-	if err != nil {
-		return links, err
+	if err := checkSheetName(sheet); err != nil {
+		return nil, err
 	}
-	if ws.Hyperlinks != nil {
-		for _, link := range ws.Hyperlinks.Hyperlink {
-			if link.RID != "" {
-				links[link.Ref] = f.getSheetRelationshipsTargetByID(sheet, link.RID)
+	name, ok := f.getSheetXMLPath(sheet)
+	if !ok {
+		return nil, ErrSheetNotExist{sheet}
+	}
+	if worksheet, ok := f.Sheet.Load(name); ok && worksheet != nil {
+		ws := worksheet.(*xlsxWorksheet)
+		ws.mu.Lock()
+		defer ws.mu.Unlock()
+		output, _ := xml.Marshal(ws)
+		f.saveFileList(name, f.replaceNameSpaceBytes(name, output))
+	}
+	_, xmlDecoder, _, err := f.xmlDecoder(name)
+	if err != nil {
+		return nil, err
+	}
+	links := make(map[string]string)
+	for {
+		token, _ := xmlDecoder.Token()
+		if token == nil {
+			return links, nil
+		}
+		switch xmlElement := token.(type) {
+		case xml.StartElement:
+			if xmlElement.Name.Local == "hyperlink" {
+				var ref, id string
+				for _, attr := range xmlElement.Attr {
+					if attr.Name.Local == "ref" && ref == "" {
+						ref = attr.Value
+					}
+					if attr.Name.Local == "id" && id == "" {
+						id = attr.Value
+					}
+				}
+				if ref != "" && id != "" {
+					links[ref] = id
+				}
+			}
+		case xml.EndElement:
+			if xmlElement.Name.Local == "hyperlinks" {
+				return links, nil
 			}
 		}
 	}
-	return links, nil
 }
